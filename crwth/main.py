b1a74ecb038d2181526977c08a8fb3660a41921c
Rickard Hultgren <sababa.sababa@gmail.com>
2017-12-21 07:36:08 +0100

    Update main.py

--- crwth/main.py
+++ crwth/main.py
@@ -13,98 +13,142 @@ class PongGame(FloatLayout):
 	inputlabel1 = NumericProperty(0)
 	inputlabel2 = NumericProperty(0)
 	inputlabel3 = NumericProperty(0)
+
+	Gvar0 = SoundLoader.load('g1.wav')
+	Gvar1 = SoundLoader.load('gis1.wav'	)
+	Gvar2 = SoundLoader.load('a1.wav')
+	Gvar3 = SoundLoader.load('ais1.wav')
+	Gvar4 = SoundLoader.load('b1.wav')
+	Gvar5 = SoundLoader.load('c2.wav')
+	Gvar6 = SoundLoader.load('cis2.wav')
+	Gvar7 = SoundLoader.load('d2.wav')
+	Gvar8 = SoundLoader.load('dis2.wav')
+	Gvar9 = SoundLoader.load('e2.wav')
+	Gvar10 = SoundLoader.load('f2.wav')
+	Gvar11 = SoundLoader.load('fis2.wav')
+	Gvar12 = SoundLoader.load('g2.wav')
+
+	Cvar0 = SoundLoader.load( 'c2.wav')
+	Cvar1 = SoundLoader.load(  'cis2.wav')
+	Cvar2 = SoundLoader.load(  'd2.wav')
+	Cvar3 = SoundLoader.load(  'dis2.wav')
+	Cvar4 = SoundLoader.load(  'e2.wav')
+	Cvar5 = SoundLoader.load(  'f2.wav')
+	Cvar6 = SoundLoader.load(  'fis2.wav')
+	Cvar7 = SoundLoader.load(  'g2.wav')
+	Cvar8 = SoundLoader.load(  'gis2.wav')
+	Cvar9 = SoundLoader.load(  'a2.wav')
+	Cvar10 = SoundLoader.load(  'ais2.wav')
+	Cvar11 = SoundLoader.load(  'b2.wav')
+	Cvar12 = SoundLoader.load(  'c3.wav')
+
+	Dvar0 = SoundLoader.load('d2.wav')
+	Dvar1 = SoundLoader.load('dis2.wav')
+	Dvar2 = SoundLoader.load('e2.wav')
+	Dvar3 = SoundLoader.load('f2.wav')
+	Dvar4 = SoundLoader.load('fis2.wav')
+	Dvar5 = SoundLoader.load('g2.wav')
+	Dvar6 = SoundLoader.load('gis2.wav')
+	Dvar7 = SoundLoader.load('a2.wav')
+	Dvar8 = SoundLoader.load('ais2.wav')
+	Dvar9 = SoundLoader.load('b2.wav')
+	Dvar10 = SoundLoader.load('c3.wav')
+	Dvar11 = SoundLoader.load('cis3.wav')
+	Dvar12 = SoundLoader.load('d3.wav')
+		
 #	sound.seek(self.inputlabel1)	
 	def update(self,dt):
 		soundG = ''
 		soundC = ''
 		soundD = ''
 		if self.inputlabel1 == 0:
-			soundG = 'g1.wav'
+			self.Gvar0.play()
 		elif self.inputlabel1 == 1:
-			soundG = 'gis1.wav'	
+			self.Gvar1.play()
 		elif self.inputlabel1 == 2:
-			soundG = 'a1.wav'
+			self.Gvar2.play()
 		elif self.inputlabel1 == 3:
-			soundG = 'ais1.wav'
+			self.Gvar3.play()
 		elif self.inputlabel1 == 4:
-			soundG = 'b1.wav'
+			self.Gvar4.play()
 		elif self.inputlabel1 == 5:
-			soundG = 'c2.wav'
+			self.Gvar5.play()
 		elif self.inputlabel1 == 6:
-			soundG = 'cis2.wav'
+			self.Gvar6.play()
 		elif self.inputlabel1 == 7:
-			soundG = 'd2.wav'
+			self.Gvar7.play()
 		elif self.inputlabel1 == 8:
-			soundG = 'dis2.wav'
+			self.Gvar8.play()
 		elif self.inputlabel1 == 9:
-			soundG = 'e2.wav'
+			self.Gvar9.play()
 		elif self.inputlabel1 == 10:
-			soundG = 'f2.wav'
+			self.Gvar10.play()
 		elif self.inputlabel1 == 11:
-			soundG = 'fis2.wav'
+			self.Gvar11.play()
 		elif self.inputlabel1 == 12:
-			soundG = 'g2.wav'
+			self.Gvar12.play()
 
 		if self.inputlabel2 == 0:
-			soundC = 'c2.wav'
+			self.Cvar0.play()
 		elif self.inputlabel2 == 1:
-			soundC = 'cis2.wav'
+			self.Cvar1.play()
 		elif self.inputlabel2 == 2:
-			soundC = 'd2.wav'
+			self.Cvar2.play()
 		elif self.inputlabel2 == 3:
-			soundC = 'dis2.wav'
+			self.Cvar3.play()
 		elif self.inputlabel2 == 4:
-			soundC = 'e2.wav'
+			self.Cvar4.play()
 		elif self.inputlabel2 == 5:
-			soundC = 'f2.wav'
+			self.Cvar5.play()
 		elif self.inputlabel2 == 6:
-			soundC = 'fis2.wav'
+			self.Cvar6.play()
 		elif self.inputlabel2 == 7:
-			soundC = 'g2.wav'
+			self.Cvar7.play()
 		elif self.inputlabel2 == 8:
-			soundC = 'gis2.wav'
+			self.Cvar8.play()
 		elif self.inputlabel2 == 9:
-			soundC = 'a2.wav'
+			self.Cvar9.play()
 		elif self.inputlabel2 == 10:
-			soundC = 'ais2.wav'
+			self.Cvar10.play()
 		elif self.inputlabel2 == 11:
-			soundC = 'b2.wav'
+			self.Cvar11.play()
 		elif self.inputlabel2 == 12:
-			soundC = 'c3.wav'
+			self.Cvar12.play()
 
 		if self.inputlabel3 == 0:
-			soundD = 'd2.wav'
+			self.Dvar0.play()
 		elif self.inputlabel3 == 1:
-			soundD = 'dis2.wav'
+			self.Dvar1.play()
 		elif self.inputlabel3 == 2:
-			soundD = 'e2.wav'
+			self.Dvar2.play()
 		elif self.inputlabel3 == 3:
-			soundD = 'f2.wav'
+			self.Dvar3.play()
 		elif self.inputlabel3 == 4:
-			soundD = 'fis2.wav'
+			self.Dvar4.play()
 		elif self.inputlabel3 == 5:
-			soundD = 'g2.wav'
+			self.Dvar5.play()
 		elif self.inputlabel3 == 6:
-			soundD = 'gis2.wav'
+			self.Dvar6.play()
 		elif self.inputlabel3 == 7:
-			soundD = 'a2.wav'
+			self.Dvar7.play()
 		elif self.inputlabel3 == 8:
-			soundD = 'ais2.wav'
+			self.Dvar8.play()
 		elif self.inputlabel3 == 9:
-			soundD = 'b2.wav'
+			self.Dvar9.play()
 		elif self.inputlabel3 == 10:
-			soundD = 'c3.wav'
+			self.Dvar10.play()
 		elif self.inputlabel3 == 11:
-			soundD = 'cis3.wav'
+			self.Dvar11.play()
 		elif self.inputlabel3 == 12:
-			soundD = 'd3.wav'
+			self.Dvar12.play()
 	
-		stringG = SoundLoader.load('%s' % soundG)
-		stringG.play()
-		stringC = SoundLoader.load('%s' % soundC)
-		stringC.play()
-		stringD = SoundLoader.load('%s' % soundD)
-		stringD.play()
+		#stringG = SoundLoader.load('%s' % soundG)
+		#stringG.play()
+		#stringG.stop()
+		#stringC = SoundLoader.load('%s' % soundC)
+		#stringC..play()
+		#stringD = SoundLoader.load('%s' % soundD)
+		#stringD.play()
 		
 class PongApp(App):
 	def build(self):
